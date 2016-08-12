4c062165824825c5b59a8626aaf44c6b20f20c8c
mimoskal <strup.parzylapa@gmail.com>
2016-08-12 20:46:41 +0200

    Add folder structure and start working on gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include plugins
+var jshint = require('gulp-jshint');
+var compass = require('gulp-compass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify-css');
+var rename = require('gulp-rename');
