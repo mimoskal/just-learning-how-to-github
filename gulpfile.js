876747c37b963d9deaeac3926b5bc16c342c7b4e
mimoskal <strup.parzylapa@gmail.com>
2016-08-14 21:09:25 +0200

    Start working on init html and css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
+/*
+@todo: 
+	** preview directory
+		sourcemaps = require('gulp-sourcemaps')
+	** clear folders
+		del = require('del')
+	** .coffee
+*/
+
 // Include gulp
 var gulp = require('gulp');
 
 // Include plugins
 var jshint = require('gulp-jshint');
-var compass = require('gulp-compass');
+var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
@@ -13,6 +22,8 @@ var notify = require('gulp-notify');
 var autoprefixer = require('gulp-autoprefixer');
 var cssShorthand = require('gulp-shorthand');
 var cleanCSS = require('gulp-clean-css');
+var livereload = require('gulp-livereload');
+var connect = require('gulp-connect');
 
 //the title and icon that will be used for the Grunt notifications
 var notifyInfo = {
@@ -39,14 +50,19 @@ gulp.task('lint', function() {
 		.pipe(jshint.reporter('default'));
 });
 
+gulp.task('connect', function() {
+  connect.server({
+    root: 'build',
+    livereload: true,
+    open: { browser: "Google Chrome"}
+  });
+});
+
 // Compass tasks
-gulp.task('compass', function() {
+gulp.task('sass', function() {
 	return gulp.src(['src/scss/**/*.scss'])
 		.pipe(plumber(plumberErrorHandler))
-		.pipe(compass({
-			css: 'src/css',
-			sass: 'src/scss'
-		}))
+		.pipe(sass())
 		.pipe(cssShorthand())
 		.pipe(autoprefixer({
             browsers: ['last 10 versions', '> 1%', 'IE 7', 'IE 8', 'IE 9', 'IE 10', 'IE 11'],
@@ -58,15 +74,23 @@ gulp.task('compass', function() {
 	        console.log(details.name+ ': ' + details.stats.originalSize);
 	        console.log(details.name+ '-min: ' + details.stats.minifiedSize);
 	    }))
-		.pipe(gulp.dest('build/css'));
+		.pipe(gulp.dest('build/css'))
+		.pipe(connect.reload());
+});
+
+gulp.task('publishHtml', function() {
+	return gulp.src(['src/**/*.html'])
+		.pipe(gulp.dest('build'))
+		.pipe(connect.reload());
 });
 
 
 // Watch files for changes
 gulp.task('watch', function() {
     gulp.watch('src/js/*.js', ['lint']);
-    gulp.watch(['src/scss/**/*.scss'], ['compass']);
+    gulp.watch(['src/scss/**/*.scss'], ['sass']);
+    gulp.watch(['src/**/*.html'], ['publishHtml']);
 });
 
 // Default task
-gulp.task('default', ['lint', 'compass', 'watch']);
+gulp.task('default', ['lint', 'sass', 'publishHtml', 'watch', 'connect']);
